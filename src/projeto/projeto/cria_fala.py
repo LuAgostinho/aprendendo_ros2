ef659b8faff3bbf3aa002fce0c9eeaffcee9e017
Lucas Agostinho Silva <114116439+LuAgostinho@users.noreply.github.com>
2024-11-17 19:35:19 +0000

    finalizado verbal

--- src/projeto/projeto/cria_fala.py
+++ src/projeto/projeto/cria_fala.py
@@ -1,56 +1,103 @@
-# pip install gTTS
-import rclpy
-from rclpy.node import Node
-from rclpy.qos import QoSProfile, QoSReliabilityPolicy
-from std_msgs.msg import String
-
 from gtts import gTTS
-tts = gTTS('Olá, eu sou um robô! Meu nome é Maria. Como posso te ajudar?', lang ="pt")
+import vlc
+import whisper
+import keyboard
+import time
+
+tts = gTTS('Olá, sou Matheus, Como posso te ajudar?', lang ="pt")
 tts.save('hello.mp3')
 
-# pip install python-vlc
-import vlc
+
 p = vlc.MediaPlayer("hello.mp3")
 p.play()
 
-
-
-class MeuNo(Node):
-
-    # Contrutor do nó
-    def __init__(self):
-
-        # Aqui é definido o nome do nó
-        super().__init__('Cria_fala')
-        qos_profile = QoSProfile(depth=10, reliability = QoSReliabilityPolicy.BEST_EFFORT)
-        self.get_logger().info('Criando as falas')
-        self.subscription = self.create_subscription(String,'verbal',self.listener_callback,qos_profile )
-       
-    # Aqui o nó é executado no ROS
-    def run(self):
-
-        # Executa uma iteração do loop de processamento de mensagens.
-        rclpy.spin(self)
-
-    # função de callback que lê a mensagem
-    def listener_callback(self, msg):
-        self.get_logger().info('Mensagem publicada: "%s"' % msg.data)
-
-    # Destrutor do nó
-    def __del__(self):
-        self.get_logger().info('Finalizando o nó! Tchau, tchau...')
-
-
-# Função principal
-def main(args=None):
-    rclpy.init(args=args)
-    node = MeuNo()
+while true:
     try:
-        node.run()
-        node.destroy_node()
-        rclpy.shutdown()
-    except KeyboardInterrupt:
-        pass
-   
-if __name__ == '__main__':
-    main()    
+        print("Aguardando entrada")
+        keyboard.wait('space')
+        time.sleep(0.2)
+        model = whisper.load_model("small")
+        result = model.transcribe("gravado.mp3", fp16=False)
+        fala = result["text"]
+        if fala == "Gostaria de uma mesa":
+            tts = gTTS('Claro, tem alguma preferência de local?', lang ="pt")
+            tts.save('resposta.mp3')
+
+            p = vlc.MediaPlayer("resposta.mp3")
+            p.play()
+
+        if fala == "Já tenho reserva":
+            tts = gTTS('Me diga seu nome por gentileza que vou guia-lós até a mesa', lang ="pt")
+            tts.save('resposta.mp3')
+
+            p = vlc.MediaPlayer("resposta.mp3")
+            p.play()
+
+        if fala == "Gostaria de fazer um pedido":
+            tts = gTTS('Beleza, me diga os items que já vou envia-los para a cozinha', lang ="pt")
+            tts.save('resposta.mp3')
+
+            p = vlc.MediaPlayer("resposta.mp3")
+            p.play()
+
+            time.sleep(5)
+
+            tts = gTTS('Seu pedido foi anotado, só aguardar que assim que estiver pronto eu trago pra vocês', lang ="pt")
+            tts.save('resposta.mp3')
+
+            p = vlc.MediaPlayer("resposta.mp3")
+            p.play()
+
+        if fala == "Eu queria minha conta por favor":
+            tts = gTTS('Certinho, vou pegar no sistema e já fechamos a sua conta', lang ="pt")
+            tts.save('resposta.mp3')
+
+            p = vlc.MediaPlayer("resposta.mp3")
+            p.play()
+
+            time.sleep(5)
+
+            tts = gTTS('Muito obrigado pela sua preferência, vou acompanhar vocês até a porta', lang ="pt")
+            tts.save('resposta.mp3')
+
+            p = vlc.MediaPlayer("resposta.mp3")
+            p.play()
+
+        if fala == "Onde fica o banheiro?":
+            tts = gTTS('O banheiro fica a direita nos fundos do restaurante', lang ="pt")
+            tts.save('resposta.mp3')
+
+            p = vlc.MediaPlayer("resposta.mp3")
+            p.play()
+        
+        if fala == "Tá demorando muito meu pedido, o que que está acontecendo?":
+            tts = gTTS('Mil desculpas a demora, vou verificar junto da cozinha e trago uma atualização para você', lang ="pt")
+            tts.save('resposta.mp3')
+
+            p = vlc.MediaPlayer("resposta.mp3")
+            p.play()
+        
+        if fala == "Como você funciona?":
+            tts = gTTS('Sou um robô para auxilio de restaurantes, tenho uma série de sensores e atuadores para ajudar em minha segurança e navegação, pode me perguntar qualquer coisa referente ao restaurante que eu te ajudo', lang ="pt")
+            tts.save('resposta.mp3')
+
+            p = vlc.MediaPlayer("resposta.mp3")
+            p.play()
+        
+        if fala == "Pode devolver meu pedido até o chefe, está cru a comida":
+            tts = gTTS('Perdão pelo transtorto, vou trazer outro pedido pra você', lang ="pt")
+            tts.save('resposta.mp3')
+
+            p = vlc.MediaPlayer("resposta.mp3")
+            p.play()
+
+            time.sleep (5)
+
+            tts = gTTS('Chefe, poderia por gentileza refazer esse pedido, o cliente reclamou de estar cru', lang ="pt")
+            tts.save('resposta.mp3')
+
+            p = vlc.MediaPlayer("resposta.mp3")
+            p.play()
+    except:
+        break
+
